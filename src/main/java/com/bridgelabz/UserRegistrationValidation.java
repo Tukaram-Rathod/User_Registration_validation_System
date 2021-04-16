409a020f560ebb6d25fe005a0727d3a2fc8ecd5c
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-16 23:42:46 +0530

    Added.validated last name

--- src/main/java/com/bridgelabz/UserRegistrationValidation.java
+++ src/main/java/com/bridgelabz/UserRegistrationValidation.java
@@ -5,24 +5,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationValidation {
+
     //First name starts with Cap and has minimum 3 characters
     public static boolean isValidFirstName(String fname) {
         // firstname pattern.
-        String FirstName_Pattern = "^[A-Z]{1}[a-z]{3,}$";
-        // Compile the ReGex
+        String FirstName_Pattern = "^[A-Z]{1}[a-z]{3,}[a-z]*$";
         Pattern pattern = Pattern.compile(FirstName_Pattern);
         Matcher matcher = pattern.matcher(fname);
-
-        if(matcher.matches())
-            System.out.println("Valid First Name !!!");
-        else
-            System.out.println("Invalid First Name !!!");
+        return matcher.matches();
+    }
+    public static boolean isValidLastName(String lname) {
+        // lastname pattern.
+        String LastName_Pattern = "^[A-Z]{1}[a-z]{3,}[a-z]*$";
+        Pattern pattern = Pattern.compile(LastName_Pattern);
+        Matcher matcher = pattern.matcher(lname);
         return matcher.matches();
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Name:");
         String firstname = sc.nextLine();
+        System.out.println("Enter Last Name:");
+        String lastname = sc.nextLine();
         System.out.println(isValidFirstName(firstname));
+        System.out.println(isValidLastName(lastname));
     }
 }
